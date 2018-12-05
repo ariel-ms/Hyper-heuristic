2551145a78b7c6f378b5d2266af3266aaaccb4bd
Ariel Mendez <arielmendez735@gmail.com>
2018-12-04 23:46:05 -0600

    First commit
    Add required files

---
+++ KP.py
@@ -0,0 +1,126 @@
+import numpy as np
+import PSO as pso
+
+MATRIX = np.array([
+    [1, 4, 40, 10],
+    [2, 7, 42, 6],
+    [3, 5, 25, 5],
+    [4, 3, 12, 4]])
+
+TOTAL_PROFIT = 0
+TOTAL_WEIGHT = 0
+CAPACITY = 10
+
+def default(matrix):
+    """
+    Select elements by the order given in the matrix
+    Returns:
+        number: weight, profit, max element index
+    """
+    return matrix[1, 1], matrix[1, 2], 1
+
+def max_profit(matrix):
+    """
+    Select element with biggest profit
+    Returns:
+        number: weight, profit, max element index
+    """
+    index_max_elem = np.argmax(matrix[:, 2], axis=0)
+    weight = matrix[index_max_elem, 1]
+    profit = matrix[index_max_elem, 2]
+    return weight, profit, index_max_elem
+
+def min_profit(matrix):
+    """
+    select element with smallest profit
+    Returns:
+        number: weight, profit, min element index
+    """
+    index_min_weight = np.argmin(matrix[:,1], axis=0)
+    weight = matrix[index_min_weight, 1]
+    profit = matrix[index_min_weight, 2]
+    return weight, profit, index_min_weight
+
+def max_profit_weight(matrix):
+    """
+    select element with best profit/weight
+    Returns:
+        number: weight, profit, max element index
+    """
+    index_max_elem = np.argmax(matrix[:, 3], axis=0)
+    weight = matrix[index_max_elem, 1]
+    profit = matrix[index_max_elem, 2]
+    return weight, profit, index_max_elem
+
+def get_avg_profit(matrix):
+    """
+    Returns: normalized average profit
+    """
+    max_val = max(matrix[:, 2])
+    avg_profit = np.mean(matrix[:, 2]) * (1/max_val)
+    return np.array([avg_profit])
+
+# set of simple heuristics
+#ACTIONS = [default, max_profit, min_profit, max_profit_weight]
+
+# SELECTOR = np.array([
+#     [0.7, max_profit_weight],
+#     [0.6, max_profit]])
+
+FEATURE = np.array([[0.7], [0.6]])
+
+SELECTOR = np.array([
+    [max_profit_weight],
+    [max_profit]
+])
+
+# euclidean distance can be calculated with dist = numpy.linalg.norm(a-b)
+def kp_hyper_heuristic(feature, selector, matrix, capacity):
+    """
+    Solves the KP using the avg profit characteritics and the rule values defined in selector
+    Returns:
+        total weight, total profit
+    """
+    selector = np.hstack((feature, selector))
+    total_profit = 0
+    total_weight = 0
+    while matrix.shape[0] != 0 and total_weight < capacity:
+        f_t = get_avg_profit(matrix) # consider flexibility for adding more characteristics
+        number_rules = selector.shape[0]
+        dist_to_rules = []
+        for i in range(number_rules):
+            rule_values = selector[i, :-1]
+            dist = np.linalg.norm(rule_values-f_t)
+            dist_to_rules.append(dist)
+
+        selected_rule = np.argmin(dist_to_rules)
+        weight, profit, index = selector[selected_rule, -1](matrix)
+
+        if total_weight + weight <= capacity:
+            total_weight += weight
+            total_profit += profit
+        matrix = np.delete(matrix, index, axis=0)
+    #return total_weight, total_profit
+    return total_profit
+
+params = [SELECTOR, MATRIX, CAPACITY]
+
+pso_instance = pso.PSO(2, 200, 100, 0.2, 0.5, 0.5, kp_hyper_heuristic, params)
+results = pso_instance.run_pso()
+print(results)
+
+#print("Initial capacity: " + str(CAPACITY))
+#val = kp_hyper_heuristic(FEATURE, SELECTOR, MATRIX, CAPACITY)
+#print("Final profit: " + str(final_profit))
+
+"""
+preguntas
+1. Cuales son los algoritmos que ajustan mejor los valores de los pesos?
+
+propuestas
+* añadir clase de caracteristicas 
+* añadir clase de reglas
+* añadir clase de KP hyper heristica que este compuesta de clase reglas
+* añadir esta clase en un modulo de Hiper Heuristicas
+"""
+
---
+++ PSO.py
@@ -0,0 +1,87 @@
+import random
+import sys
+import numpy as np
+
+# WK = 0.9
+# C1 = 10
+# C2 = 10
+
+# def func1(x):
+#     # total = 0
+#     # for i in range(len(x)):
+#     #     total += x[i]**2
+#     # return total
+#     return np.sum(x**2)
+
+class Particle:
+    def __init__(self, NUM_DIM, WK, C1, C2):
+        self.NUM_DIM = NUM_DIM
+        self.WK = WK
+        self.C1 = C1
+        self.C2 = C2
+        self.position = np.random.rand(NUM_DIM, 1)
+        self.velocity = np.random.rand(NUM_DIM, 1)
+        self.best_position = np.array([])
+        self.best_error = sys.maxsize
+
+        # for dim in range(0, NUM_DIM):
+        #     self.position.append(random.random())
+        #     self.velocity.append(0.01 * random.random())
+
+    def evaluate_function(self, cost_function, params):
+        error = cost_function(self.position, *params)
+        if error < self.best_error:
+            self.best_position = self.position
+            self.best_error = error
+
+    def update_velocity(self, best_global_position):
+        R1 = np.random.rand(self.NUM_DIM,1)
+        R2 = np.random.rand(self.NUM_DIM,1)
+        self.velocity = self.WK*self.velocity + self.C1*R1*(self.best_position - self.position) + self.C2*R2*(best_global_position - self.position)
+
+    def update_position(self):
+        self.position = self.position + self.velocity
+
+class PSO():
+    def __init__(self, num_dim, num_particles, max_iter, WK, C1, C2, cost_function, function_params):
+        self.num_dim = num_dim
+        self.num_particles = num_particles
+        self.max_iter = max_iter
+        self.cost_function = cost_function
+        self.params = function_params
+        self.WK = WK
+        self.C1 = C1
+        self.C2 = C2
+        self.swarm = []
+        self.best_global_error = sys.maxsize
+        self.best_global_position = np.array([])
+        self.num_particles = num_particles
+
+    def run_pso(self):
+        for each_particle in range(0, self.num_particles):
+            particle = Particle(self.num_dim, self.WK, self.C1, self.C2)
+            self.swarm.append(particle)
+
+        for iteration in range(0, self.max_iter):
+
+            for particle in self.swarm:
+                particle.evaluate_function(self.cost_function, self.params)
+
+                # PREGUNTAR SI EL BEST DE ESTA PARTICULA ES LMEJOR QUE EL BEST GLOBALS
+                if particle.best_error <  self.best_global_error:
+                    best_global_error = particle.best_error
+                    best_global_position = particle.position
+
+            for particle in self.swarm:
+                particle.update_velocity(best_global_position)
+                particle.update_position()
+
+        # print(best_global_position)
+        # print(best_global_error)
+        return (best_global_position, best_global_error)
+
+# def main():
+#     particle = PSO(2, 3, 3, func1, 0.9, 10, 10)
+#     values = particle.run_pso()
+
+# main()
